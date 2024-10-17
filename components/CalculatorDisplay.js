4e93764e4cc9bf0be5413663a15f5bdf6ff128f2
Luke Gaunt <30408050+FinchyG@users.noreply.github.com>
2024-10-17 18:52:13 +0100

    indent correction

--- components/CalculatorDisplay.js
+++ components/CalculatorDisplay.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function CalculatorDisplay(props) {
   
-    return (
-        <div id="displayDiv">
-            <p id="displayP">
-                {props.displayValue}
-            </p>
-        </div>
-    );
-  }
+  return (
+    <div id="displayDiv">
+      <p id="displayP">
+          {props.displayValue}
+      </p>
+    </div>
+  );
+}
 
-  export default CalculatorDisplay;
+export default CalculatorDisplay;
--- components/NumberButton.js
+++ components/NumberButton.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 function NumberButton(props) {
   
-    return (
-        <button 
-            className={props.className}
-            onClick={props.onClick}
-        >
-            {props.num}
-        </button>
-    );
-  }
+  return (
+    <button 
+      className={props.className}
+      onClick={props.onClick}
+    >
+      {props.num}
+    </button>
+  );
+}
 
-  export default NumberButton;
+export default NumberButton;
